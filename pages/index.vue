96accbfa12fbef54e3f68635553e403988c4ead0
Michał Szapiel <szapiel104@gmail.com>
2021-11-19 17:33:11 +0100

    Test commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,116 +1,87 @@
 <template>
   <div class="container">
-    <div>
-      <Logo/>
-      <h1 class="title">
-        Łódzkie Alpaki
-      </h1>
-      <h3 class="subtitle">
-        Strona w budowie!
-      </h3>
-      <hr/>
-      <a href="https://www.instagram.com/lodzkiealpaki/"
-         class="text-decoration-none"
-         target="_blank">
-        <div class="d-flex align-items-center justify-content-center">
-          <p class="mr-3 mb-0">Zajrzyj do nas na Insta!</p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#dfb5a6" class="bi bi-instagram"
-               viewBox="0 0 16 16">
-            <path
-              d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
-          </svg>
+    <div class="row my-5 d-flex justify">
+      <div class="col-12 col-md-6 my-auto">
+        <h1>Łódzkie alpaki</h1>
+        <p class="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla leo eu sapien
+          aliquam, hendrerit facilisis libero pretium.</p>
+        <div class="row">
+          <div class="col-12 col-md-4">
+            <b-button size="sm" class="my-2 a-link-btn normal" type="primary" variant="primary" to="/harmonogram">
+              <span class="mr-1">Oferta</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
+                   class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                      d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+              </svg>
+            </b-button>
+          </div>
+          <div class="col-12 col-md-4">
+            <b-button size="sm" class="my-2 a-link-btn white" type="primary" variant="primary" to="/kontakt">
+              <span class="mr-1">Kontakt</span>
+            </b-button>
+          </div>
         </div>
-      </a>
+
+      </div>
+      <div class="col-12 col-md-6">
+        <img src="~assets/img/erik-dungan-lNUi7W4iwok-unsplash.jpg" alt="" class="img-fluid"
+             style="border-radius: 10px">
+      </div>
+    </div>
+    <div class="row my-5 d-flex justify">
+      <div class="col-12">
+        <h1 class="mb-3">Znajdź nas</h1>
+        <div id="map-wrap" style="height: 600px">
+          <no-ssr>
+            <l-map :zoom=13 :center="[51.7034009,19.4283212]">
+              <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+              <l-marker :lat-lng="[51.7034009,19.4283212]"><l-popup><a href="#">Lokalizacja (link do google maps)</a></l-popup></l-marker>
+              <l-marker :lat-lng="[51.6934001,19.4283212]"><l-popup><a href="#">Lokalizacja (link do google maps)</a></l-popup></l-marker>
+            </l-map>
+          </no-ssr>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+
+
 export default {
+  data() {
+    return {
+      // center: {lat: 51.6054142, lng: 19.4996098},
+      // markers:[
+      //   {
+      //     position: {lat: 10.0, lng: 10.0}
+      //   },
+      //   {
+      //     position: {lat: 10.0, lng: 10.0}
+      //   }
+      // ]
+    }
+  },
   head() {
     return {
-      title: 'Łódzkie Alpaki - Strona w budowie',
+      title: 'Łódzkie alpaki',
       meta: [
         {charset: 'utf-8'},
         {name: 'viewport', content: 'width=device-width, initial-scale=1'},
         {
           hid: 'description',
           name: 'description',
-          content: 'Łódzkie Alpaki'
+          content: 'Łódzkie alpaki'
         }
       ],
     }
-  }
+  },
+
+
 }
 </script>
-
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-@media (max-width: 992px) {
-  .title {
-    font-size: 50px;
-  }
-
-  .subtitle {
-    font-size: 20px;
-  }
-}
-
-@media (min-width: 993px) {
-  .title {
-    font-size: 100px;
-  }
-
-  .subtitle {
-    font-size: 42px;
-  }
-}
-
-.title {
-  font-family: 'Quicksand',
-  'Source Sans Pro',
-  -apple-system,
-  BlinkMacSystemFont,
-  'Segoe UI',
-  Roboto,
-  'Helvetica Neue',
-  Arial,
-  sans-serif;
-  display: block;
-  font-weight: 300;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-p {
-  font-family: 'Quicksand',
-  'Source Sans Pro',
-  -apple-system,
-  BlinkMacSystemFont,
-  'Segoe UI',
-  Roboto,
-  'Helvetica Neue',
-  Arial,
-  sans-serif;
-  color: #526488;
-}
-
-.links {
-  padding-top: 15px;
+.leaflet-popup-content-wrapper{
 }
 </style>
